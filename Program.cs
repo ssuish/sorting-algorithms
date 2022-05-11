040548cc95a288bb7da3217b93e9cadf9d19fb01
suisoh <fubuki.k016@gmail.com>
2022-05-11 22:03:28 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Sorting Algorithms in C#
+
+/*
+Create a complete C# program that allows user to input a series of integers and arrange 
+these in ascending and descending order applying the logical concepts involved in the 
+following sorting algorithms: Insertion Sort, Selection Sort or Merge Sort.
+*/
+
+int[] numArr = new int[10];
+
+for (int i = 0; i < numArr.Length; i++)
+{
+    Console.Write("Input a series of integers (up to 10 intergers only): ");
+    numArr[i] = int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Insertion Sort 
+
