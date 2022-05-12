62db0f5f79a9363cdc145d8a434f184868beaee2
suisoh <fubuki.k016@gmail.com>
2022-05-12 15:24:17 +0800

    insertion sort

--- Program.cs
+++ Program.cs
@@ -14,9 +14,39 @@ for (int i = 0; i < numArr.Length; i++)
     numArr[i] = int.Parse(Console.ReadLine() ?? "0");
 }
 
+sort(numArr);
+Console.WriteLine("Sorted Array: ");
+printArray(numArr);
+
 // Insertion Sort
+void sort(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 1; i < n; ++i)
+    {
+        int key = arr[i];
+        int j = i - 1;
 
+        // Move elements of arr[0..i-1],
+        // that are greater than key,
+        // to one position ahead of
+        // their current position
 
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; ++i)
+        Console.Write(arr[i] + " ");
+}
 
 // Selection Sort
 
