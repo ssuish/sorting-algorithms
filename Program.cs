0742457a7142358a1c5f8cbae58f538d7dae1090
suisoh <fubuki.k016@gmail.com>
2022-05-14 15:36:50 +0800

    final

--- Program.cs
+++ Program.cs
@@ -15,23 +15,35 @@ for (int i = 0; i < numArr.Length; i++)
 
 while (true)
 {
-    Console.WriteLine("\nChoose a sorting method.\n [0]Insertion Sort\n [1]Selectin Sort\n [2]Merge Sort");
+    Console.WriteLine("\nChoose a sorting method.\n [0]Insertion Sort\n [1]Selection Sort\n [2]Merge Sort");
     Console.Write("\nYour respoonse: ");
     int choosen = int.Parse(Console.ReadLine() ?? "3");
 
     if (choosen == 0)
     {
-        sort(numArr);
-        Console.WriteLine("Sorted Array using Insertion method: ");
+        Console.WriteLine("Array: ");
+        printArray(numArr);
+        InsertionSort(numArr);
+        Console.WriteLine("Sorted Array using Insertion Sort method: ");
         printArray(numArr);
         break;
     }
     else if (choosen == 1)
     {
+        Console.WriteLine("Array: ");
+        printArray(numArr);
+        SelectionSort(numArr);
+        Console.WriteLine("Sorted Array using Selection Sort method: ");
+        printArray(numArr);
         break;
     }
     else if (choosen == 2)
     {
+        Console.WriteLine("Array: ");
+        printArray(numArr);
+        MergeSort(numArr, 0, numArr.Length - 1);
+        Console.WriteLine("Sorted Array using Merge Sort method: ");
+        printArray(numArr);
         break;
     }
     else
@@ -40,10 +52,14 @@ while (true)
     }
 }
 
-
+void printArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; ++i)
+        Console.Write(arr[i] + " ");
+}
 
 // Insertion Sort
-void sort(int[] arr)
+void InsertionSort(int[] arr)
 {
     int n = arr.Length;
     for (int i = 1; i < n; ++i)
@@ -66,13 +82,106 @@ void sort(int[] arr)
     }
 }
 
-void printArray(int[] arr)
+// Selection Sort
+static void SelectionSort(int[] arr)
 {
-    for (int i = 0; i < arr.Length; ++i)
-        Console.Write(arr[i] + " ");
+    int n = arr.Length;
+
+    // One by one move boundary of unsorted subarray
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Find the minimum element in unsorted array
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element with the first
+        // element
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
 }
 
-// Selection Sort
-
-
 // Merge Sort
+
+void merge(int[] arr, int l, int m, int r)
+{
+    // Find sizes of two
+    // subarrays to be merged
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    int[] L = new int[n1];
+    int[] R = new int[n2];
+    int i, j;
+
+    // Copy data to temp arrays
+    for (i = 0; i < n1; ++i)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; ++j)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays
+
+    // Initial indexes of first
+    // and second subarrays
+    i = 0;
+    j = 0;
+
+    // Initial index of merged
+    // subarray array
+    int k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements
+    // of L[] if any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy remaining elements
+    // of R[] if any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void MergeSort(int[] arr, int l, int r)
+{
+    if (l < r)
+    {
+        // Find the middle
+        // point
+        int m = l + (r - l) / 2;
+
+        // Sort first and
+        // second halves
+        MergeSort(arr, l, m);
+        MergeSort(arr, m + 1, r);
+
+        // Merge the sorted halves
+        merge(arr, l, m, r);
+    }
+}
