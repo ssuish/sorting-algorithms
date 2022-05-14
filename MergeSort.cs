f4fe68c57a554fa03c1502d434524c154fe944df
suisoh <fubuki.k016@gmail.com>
2022-05-14 19:00:33 +0800

    final merge

--- MergeSort.cs
+++ MergeSort.cs
@@ -50,7 +50,55 @@ public class MergeSort
         }
     }
 
-    void MergeSortAsc(int[] arr, int l, int r) // ascending order
+    void mergeDes(int[] arr, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j;
+
+        for (i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        i = 0;
+        j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] >= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void MergeSortAsc(int[] arr, int l, int r) // ascending order
     {
         if (l < r)
         {
@@ -59,6 +107,18 @@ public class MergeSort
             MergeSortAsc(arr, l, m);
             MergeSortAsc(arr, m + 1, r);
             merge(arr, l, m, r);
+        }
+    }
+
+    public void MergeSortDes(int[] arr, int l, int r) // ascending order
+    {
+        if (l < r)
+        {
+            int m = l + (r - l) / 2;
+
+            MergeSortDes(arr, l, m);
+            MergeSortDes(arr, m + 1, r);
+            mergeDes(arr, l, m, r);
         }
     }
 }
