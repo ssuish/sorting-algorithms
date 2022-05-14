ae56fa6e69ba242ec9bfa85927f018ae05392fa2
suisoh <fubuki.k016@gmail.com>
2022-05-14 20:02:23 +0800

    final

--- MergeSort.cs
+++ MergeSort.cs
@@ -2,7 +2,7 @@ public class MergeSort
 {
     // Merge Sort
 
-    void merge(int[] arr, int l, int m, int r)
+    void merge(int[] arr, int l, int m, int r, int a)
     {
         int n1 = m - l + 1;
         int n2 = r - m;
@@ -22,16 +22,33 @@ public class MergeSort
         int k = l;
         while (i < n1 && j < n2)
         {
-            if (L[i] <= R[j])
+            if (a == 0)
             {
-                arr[k] = L[i];
-                i++;
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
             }
-            else
+            else if (a == 1)
             {
-                arr[k] = R[j];
-                j++;
+                if (L[i] >= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
             }
+
             k++;
         }
 
@@ -50,75 +67,15 @@ public class MergeSort
         }
     }
 
-    void mergeDes(int[] arr, int l, int m, int r)
-    {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        int i, j;
-
-        for (i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-
-        i = 0;
-        j = 0;
-
-        int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] >= R[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-    public void MergeSortAsc(int[] arr, int l, int r) // ascending order
+    public void MergeSorting(int[] arr, int l, int r, int a) // ascending order
     {
         if (l < r)
         {
             int m = l + (r - l) / 2;
 
-            MergeSortAsc(arr, l, m);
-            MergeSortAsc(arr, m + 1, r);
-            merge(arr, l, m, r);
-        }
-    }
-
-    public void MergeSortDes(int[] arr, int l, int r) // ascending order
-    {
-        if (l < r)
-        {
-            int m = l + (r - l) / 2;
-
-            MergeSortDes(arr, l, m);
-            MergeSortDes(arr, m + 1, r);
-            mergeDes(arr, l, m, r);
+            MergeSorting(arr, l, m, a);
+            MergeSorting(arr, m + 1, r, a);
+            merge(arr, l, m, r, a);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,35 +15,78 @@ for (int i = 0; i < numArr.Length; i++)
 
 while (true)
 {
-    Console.WriteLine("\nChoose a sorting method.\n [0]Insertion Sort\n [1]Selection Sort\n [2]Merge Sort");
+    Console.WriteLine("\nChoose a sorting method.\n [0] Insertion Sort\n [1] Selection Sort\n [2] Merge Sort");
     Console.Write("\nYour respoonse: ");
     int choosen = int.Parse(Console.ReadLine() ?? "3");
 
+    Console.WriteLine("\r\nChoose arrangement. [0] Ascending [1] Descending");
+    Console.Write("\nYour response: ");
+    int arrange = int.Parse(Console.ReadLine() ?? "3");
+
     if (choosen == 0)
     {
-        Console.WriteLine("\nArray: ");
+        Console.Write("\r\nUnsorted Array: ");
         printArray(numArr);
-        InsertionSort(numArr);
-        Console.WriteLine("\nSorted Array using Insertion Sort method: ");
+        InsertionSort insertion = new InsertionSort();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        if (arrange == 0)
+        {
+            insertion.InsertionSortAsc(numArr);
+            Console.WriteLine("\nSorted Array using Insertion Sort method (Ascending): ");
+        }
+        else if (arrange == 1)
+        {
+            insertion.InsertionSortDes(numArr);
+            Console.WriteLine("\nSorted Array using Insertion Sort method (Descending): ");
+        }
+
         printArray(numArr);
+        Console.ResetColor();
         break;
     }
     else if (choosen == 1)
     {
-        Console.WriteLine("\nArray: ");
+        Console.Write("\r\nUnsorted Array: ");
         printArray(numArr);
-        SelectionSort(numArr);
-        Console.WriteLine("\nSorted Array using Selection Sort method: ");
+        SelectionSort selection = new SelectionSort();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        if (arrange == 0)
+        {
+            selection.SelectionSortAsc(numArr);
+            Console.WriteLine("\nSorted Array using Selection Sort method (Ascending): ");
+        }
+        else if (arrange == 1)
+        {
+            selection.SelectionSortDes(numArr);
+            Console.WriteLine("\nSorted Array using Selection Sort method (Descending): ");
+        }
+
         printArray(numArr);
+        Console.ResetColor();
         break;
     }
     else if (choosen == 2)
     {
-        Console.WriteLine("\nArray: ");
+        Console.Write("\r\nUnsorted Array: ");
         printArray(numArr);
-        MergeSort(numArr, 0, numArr.Length - 1);
-        Console.WriteLine("\nSorted Array using Merge Sort method: ");
+        MergeSort merge = new MergeSort();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        if (arrange == 0)
+        {
+            merge.MergeSorting(numArr, 0, numArr.Length - 1, arrange);
+            Console.Write("\r\nSorted Array using Merge Sort (Ascending): ");
+        }
+        else if (arrange == 1)
+        {
+            merge.MergeSorting(numArr, 0, numArr.Length - 1, arrange);
+            Console.Write("\r\nSorted Array using Merge Sort (Descending): ");
+        }
+
         printArray(numArr);
+        Console.ResetColor();
         break;
     }
     else
@@ -56,102 +99,4 @@ void printArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; ++i)
         Console.Write(arr[i] + " ");
-}
-
-// Insertion Sort
-void InsertionSort(int[] arr)
-{
-    int n = arr.Length;
-    for (int i = 1; i < n; ++i)
-    {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-
-        arr[j + 1] = key;
-    }
-}
-
-// Selection Sort
-static void SelectionSort(int[] arr)
-{
-    int n = arr.Length;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-    }
-}
-
-// Merge Sort
-
-void merge(int[] arr, int l, int m, int r)
-{
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    int[] L = new int[n1];
-    int[] R = new int[n2];
-    int i, j;
-
-    for (i = 0; i < n1; ++i)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; ++j)
-        R[j] = arr[m + 1 + j];
-
-    i = 0;
-    j = 0;
-
-    int k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void MergeSort(int[] arr, int l, int r)
-{
-    if (l < r)
-    {
-        int m = l + (r - l) / 2;
-
-        MergeSort(arr, l, m);
-        MergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
 }
