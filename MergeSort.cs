e29668d7a064f34c9b4dca0df1c41a7fc67ff747
suisoh <fubuki.k016@gmail.com>
2022-05-14 17:59:41 +0800

    seperated by class

---
+++ MergeSort.cs
@@ -0,0 +1,64 @@
+public class MergeSort
+{
+    // Merge Sort
+
+    void merge(int[] arr, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j;
+
+        for (i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        i = 0;
+        j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void MergeSortAsc(int[] arr, int l, int r) // ascending order
+    {
+        if (l < r)
+        {
+            int m = l + (r - l) / 2;
+
+            MergeSortAsc(arr, l, m);
+            MergeSortAsc(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+}
---
+++ SelectionSort.cs
@@ -0,0 +1,19 @@
+public class SelectionSort
+{
+    // Selection Sort
+    static void SelectionSortAsc(int[] arr) // ascending order
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
