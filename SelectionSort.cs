658aba121159e7dc4e55c388c3bec2225d7b5385
suisoh <fubuki.k016@gmail.com>
2022-05-14 18:17:17 +0800

    final selection sort

--- SelectionSort.cs
+++ SelectionSort.cs
@@ -1,7 +1,7 @@
 public class SelectionSort
 {
     // Selection Sort
-    static void SelectionSortAsc(int[] arr) // ascending order
+    public void SelectionSortAsc(int[] arr) // ascending order
     {
         int n = arr.Length;
 
@@ -13,6 +13,23 @@ public class SelectionSort
                     min_idx = j;
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public void SelectionSortDes(int[] arr) // ascending order
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int max_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] > arr[max_idx])
+                    max_idx = j;
+
+            int temp = arr[max_idx];
+            arr[max_idx] = arr[i];
             arr[i] = temp;
         }
     }
