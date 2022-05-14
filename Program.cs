26ff5fbdc8ecc1c8bb3781c5e279e52d978ef358
suisoh <fubuki.k016@gmail.com>
2022-05-14 15:43:02 +0800

    final2

--- Program.cs
+++ Program.cs
@@ -21,28 +21,28 @@ while (true)
 
     if (choosen == 0)
     {
-        Console.WriteLine("Array: ");
+        Console.WriteLine("\nArray: ");
         printArray(numArr);
         InsertionSort(numArr);
-        Console.WriteLine("Sorted Array using Insertion Sort method: ");
+        Console.WriteLine("\nSorted Array using Insertion Sort method: ");
         printArray(numArr);
         break;
     }
     else if (choosen == 1)
     {
-        Console.WriteLine("Array: ");
+        Console.WriteLine("\nArray: ");
         printArray(numArr);
         SelectionSort(numArr);
-        Console.WriteLine("Sorted Array using Selection Sort method: ");
+        Console.WriteLine("\nSorted Array using Selection Sort method: ");
         printArray(numArr);
         break;
     }
     else if (choosen == 2)
     {
-        Console.WriteLine("Array: ");
+        Console.WriteLine("\nArray: ");
         printArray(numArr);
         MergeSort(numArr, 0, numArr.Length - 1);
-        Console.WriteLine("Sorted Array using Merge Sort method: ");
+        Console.WriteLine("\nSorted Array using Merge Sort method: ");
         printArray(numArr);
         break;
     }
@@ -67,11 +67,6 @@ void InsertionSort(int[] arr)
         int key = arr[i];
         int j = i - 1;
 
-        // Move elements of arr[0..i-1],
-        // that are greater than key,
-        // to one position ahead of
-        // their current position
-
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
@@ -87,17 +82,12 @@ static void SelectionSort(int[] arr)
 {
     int n = arr.Length;
 
-    // One by one move boundary of unsorted subarray
     for (int i = 0; i < n - 1; i++)
     {
-        // Find the minimum element in unsorted array
         int min_idx = i;
         for (int j = i + 1; j < n; j++)
             if (arr[j] < arr[min_idx])
                 min_idx = j;
-
-        // Swap the found minimum element with the first
-        // element
         int temp = arr[min_idx];
         arr[min_idx] = arr[i];
         arr[i] = temp;
@@ -108,31 +98,21 @@ static void SelectionSort(int[] arr)
 
 void merge(int[] arr, int l, int m, int r)
 {
-    // Find sizes of two
-    // subarrays to be merged
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
     int[] L = new int[n1];
     int[] R = new int[n2];
     int i, j;
 
-    // Copy data to temp arrays
     for (i = 0; i < n1; ++i)
         L[i] = arr[l + i];
     for (j = 0; j < n2; ++j)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays
-
-    // Initial indexes of first
-    // and second subarrays
     i = 0;
     j = 0;
 
-    // Initial index of merged
-    // subarray array
     int k = l;
     while (i < n1 && j < n2)
     {
@@ -149,8 +129,6 @@ void merge(int[] arr, int l, int m, int r)
         k++;
     }
 
-    // Copy remaining elements
-    // of L[] if any
     while (i < n1)
     {
         arr[k] = L[i];
@@ -158,8 +136,6 @@ void merge(int[] arr, int l, int m, int r)
         k++;
     }
 
-    // Copy remaining elements
-    // of R[] if any
     while (j < n2)
     {
         arr[k] = R[j];
@@ -172,16 +148,10 @@ void MergeSort(int[] arr, int l, int r)
 {
     if (l < r)
     {
-        // Find the middle
-        // point
         int m = l + (r - l) / 2;
 
-        // Sort first and
-        // second halves
         MergeSort(arr, l, m);
         MergeSort(arr, m + 1, r);
-
-        // Merge the sorted halves
         merge(arr, l, m, r);
     }
 }
